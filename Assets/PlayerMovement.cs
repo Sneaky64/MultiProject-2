397f55998b8f7e971cdee9e9c7eb06fec2bf8320
Sneaky640 <jordwc@gmail.com>
2023-04-28 13:21:10 +1000

    InitialCommit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables, Awake and Start
+    #endregion
+    #region Update, FixedUpdate
+    #endregion
+    #region Custom Functions
+    #endregion
+}
