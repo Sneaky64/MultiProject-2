f31bf6beb2097741cfabbeb44ba348227a8ac4ee
Sneaky640 <jordwc@gmail.com>
2023-07-20 10:10:31 +1000

    ui
    ui stuff

--- Assets/Scripts/UI/ButtonHover.cs
+++ Assets/Scripts/UI/ButtonHover.cs
@@ -5,24 +5,31 @@ using TMPro;
 
 public class ButtonHover : MonoBehaviour
 {
-	public float scaleFactor;
-	public float scaleTime;
-	float fontSize_;
+	//public float scaleFactor;
+	//public float scaleTime;
+	//float fontSize_;
 
-	TMP_Text text;
+	//TMP_Text text;
+
+	public GameObject lightOff;
+	public GameObject lightOn;
 
 	private void Start()
 	{
-		text = GetComponentInChildren<TMP_Text>();
-		fontSize_ = text.fontSize;
+		//text = GetComponentInChildren<TMP_Text>();
+		//fontSize_ = text.fontSize;
 	}
-	public void ScalingMouseOver()
+	public void MouseOver()
 	{
-		LeanTween.value(fontSize_, fontSize_ * scaleFactor, scaleTime).setEaseOutQuart().setOnUpdate(TweenText);
+		lightOff.SetActive(false);
+		lightOn.SetActive(true);
+		//LeanTween.value(fontSize_, fontSize_ * scaleFactor, scaleTime).setEaseOutQuart().setOnUpdate(TweenText);
 	}
-	public void ScalingMouseOut()
+	public void MouseOut()
 	{
-		LeanTween.value(text.fontSize, fontSize_, scaleTime).setEaseOutQuart().setOnUpdate(TweenText);
+		lightOff.SetActive(true);
+		lightOn.SetActive(false);
+		//LeanTween.value(text.fontSize, fontSize_, scaleTime).setEaseOutQuart().setOnUpdate(TweenText);
 	}
 	public void TweenText(float size)
 	{
